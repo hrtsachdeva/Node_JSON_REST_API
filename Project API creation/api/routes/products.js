1e15096e033ee66636c4efcbb2eaa217e28f8af0
hrtsachdeva <hrtsachdeva@gmail.com>
2019-02-08 08:08:37 +0530

    added all api

---
+++ Project API creation/api/routes/products.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/',(req ,res,next) =>
+{
+    res.status(200).json({
+        message: 'handling get request '
+    });
+} );
+
+router.post('/',(req ,res,next) =>
+{
+    // const product ={
+    //     name: req.body.name
+    // };
+    res.status(200).json({
+        message: 'handling post request ',
+        // data:product
+    });
+} );
+
+router.get('/:productid',(req,res,next) => {
+    const id = req.params.productid;
+    if(id == 'special')
+    {
+        res.status(200).json({
+            message:'search success'
+        });
+    
+    }else{
+        res.status(200).json({
+            message:'search failed'
+        });
+    }
+    
+});
+
+
+router.patch('/:productid',(req,res,next) => {
+    const id = req.params.productid;
+    if(id == 'special')
+    {
+        res.status(200).json({
+            message:'patch success'
+        });
+    
+    }else{
+        res.status(200).json({
+            message:'patch failed'
+        });
+    }
+    
+});
+
+
+router.delete('/:productid',(req,res,next) => {
+    const id = req.params.productid;
+    if(id == 'special')
+    {
+        res.status(200).json({
+            message:'delete success'
+        });
+    
+    }else{
+        res.status(200).json({
+            message:'delete failed'
+        });
+    }
+    
+});
+
+module.exports = router;
